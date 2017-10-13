9b4791b29603c5c88f43efd7a27e9bee4a18a397
radha1972 <32772334+radha1972@users.noreply.github.com>
2017-10-13 10:45:57 -0400

    Add files via upload

---
+++ Fib.java
@@ -0,0 +1,22 @@
+public class Fib
+{
+     public static void main(String args[])
+      {
+        int x=0,temp=0;
+        int y=1;
+        int n=20;
+        int i=0;
+System.out.println("the values in fibonacci series is");
+    for (i=0;i<=n;i++)
+       {
+
+       System.out.println("radha"+x);
+        temp=x;
+        x=y;
+        y=x+temp;
+}
+
+
+
+}
+}
